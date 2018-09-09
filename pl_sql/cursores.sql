ad81e7868630de40b51b552eeba2d8440bebdcde
JefferLindao <jefferlindao@gmail.com>
2018-09-09 10:38:46 -0500

    Ejemplo de cursor lista de estudiante por especialidad

--- pl_sql/cursores.sql
+++ pl_sql/cursores.sql
@@ -9,3 +9,38 @@ BEGIN
   		||':'||r_students.current_credits);
   END LOOP;
 END;
+
+/*ejemplo de cursor pasando parametros*/
+DECLARE
+CURSOR c_students(p_major varchar2) IS
+	SELECT first_name, last_name, current_credits
+	FROM students 
+	WHERE major = p_major;
+BEGIN
+  FOR r_students IN c_students('History') LOOP
+  	dbms_output.put_line(r_students.first_name||' '||r_students.last_name
+  		||':'||r_students.current_credits);
+  END LOOP;
+END;
+
+
+/*ejemplos de cursores Explicito Imprimir la lista de estudiente por especialidad*/
+DECLARE
+CURSOR c_major IS
+	SELECT distinct major
+	FROM students;
+
+CURSOR c_students(p_major varchar2) IS
+	SELECT first_name, last_name, current_credits
+	FROM students 
+	WHERE major = p_major;
+BEGIN
+	FOR r_major IN c_major LOOP
+		dbms_output.put_line(r_major.major);
+		FOR r_students IN c_students(r_major.major) LOOP
+			dbms_output.put_line(r_students.first_name||' '||r_students.last_name
+				||':'||r_students.current_credits);
+			dbms_output.put_line('');
+		END LOOP;
+	END LOOP;
+END;
