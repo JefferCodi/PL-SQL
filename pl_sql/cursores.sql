51e71385ecc32c58e108cd84bb688310c862f240
JefferLindao <jefferlindao@gmail.com>
2018-09-09 10:55:32 -0500

    elejemolo de cursor aumentar tres creaditos a los estudintes de una carrera especifica

--- pl_sql/cursores.sql
+++ pl_sql/cursores.sql
@@ -44,3 +44,39 @@ BEGIN
 		END LOOP;
 	END LOOP;
 END;
+
+
+/*
+   Aumentar tres creditos a los alumnos de una carrera en especifico
+   Mostrar los datos de los alumnos antes y despues.
+*/
+DECLARE
+CURSOR c_major IS
+	SELECT distinct major
+	FROM students
+	WHERE major = 'Computer Science';
+
+CURSOR c_students(p_major varchar2) IS
+	SELECT id, first_name, last_name, current_credits
+	FROM students 
+	WHERE major = p_major;
+	/*este for nowait si llama a este cursor una segunda ves 
+	  y si esta en proceso lo bloquera y enviara un mensaje 
+	  de espera porq ese esta ejecutando */
+	FOR UPDATE nowait;
+BEGIN
+	FOR r_major IN c_major LOOP
+		dbms_output.put_line(r_major.major);
+		FOR r_students IN c_students(r_major.major) LOOP
+			dbms_output.put_line(r_students.first_name||' '||r_students.last_name
+				||':'||r_students.current_credits);
+			UPDATE students SET current_credits = current_credits + 3 
+			WHERE id = r_students.id;
+		END LOOP;
+		FOR r_students IN c_students(r_major.major) LOOP
+			dbms_output.put_line(r_students.first_name||' '||r_students.last_name
+				||':'||r_students.current_credits);
+			dbms_output.put_line('');
+		END LOOP;
+	END LOOP;
+END;
